bdaac3ff0dbc2ec17497dcf010d03eac61d742bd
Jakub Kąkolewski <jkakolewski@sii.pl>
2019-07-18 11:46:51 +0200

    test properties added for the test spring context

--- src/main/java/pl/kuba565/WikiTransformerApplication.java
+++ src/main/java/pl/kuba565/WikiTransformerApplication.java
@@ -1,13 +1,18 @@
 package pl.kuba565;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class WikiTransformerApplication {
+public class WikiTransformerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(WikiTransformerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
     }
 }
